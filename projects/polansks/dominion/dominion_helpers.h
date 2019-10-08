193ba882f5c1b04a447b0dcd458ccba428584a33
Scott <polansks@oregonstate.edu>
2019-10-07 21:23:43 -0600

    Assignment 2 - Refactor (no bugs)
    Refactored the code according to assignment 2 instructions. Have not introduced bugs yet. Additional refactoring possible.

--- projects/polansks/dominion/dominion_helpers.h
+++ projects/polansks/dominion/dominion_helpers.h
@@ -11,5 +11,9 @@ int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
 int getCost(int cardNumber);
 int cardEffect(int card, int choice1, int choice2, int choice3,
                struct gameState *state, int handPos, int *bonus);
-
+int playAmbassador(int choice1, int choice2, struct gameState *state, int handPos);
+int playMine(int choice1, int choice2, struct gameState *state);
+int playMinion(int choice1, int choice2, struct gameState *state, int handPos);
+int playTribute(struct gameState *state);
+int playBaron(int choice1, struct gameState *state)
 #endif
